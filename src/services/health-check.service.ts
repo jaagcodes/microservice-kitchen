96502a2cb49afcf060aeaaa3c37a8eb6dc24c9c1
Jose Alejandro <jose.arrauttg@pragma.com.co>
2024-06-11 11:06:41 -0500

    euphoric up and running!

--- src/services/health-check.service.ts
+++ src/services/health-check.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class HealthCheckService {
   getHealthStatus(): string {
-    return 'up and running';
+    return 'up and running!!!!';
   }
 }
